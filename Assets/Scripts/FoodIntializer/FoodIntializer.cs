50ca76dec332cdf08a8bf2bb8d4aa0f424522b42
YichengLiPaul <yichengl@umich.edu>
2019-10-27 20:44:21 -0400

    Make a food set

---
+++ Assets/Scripts/FoodIntializer/FoodIntializer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodIntializer : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        FoodBehavior fb = GetComponent<FoodBehavior>();
+        fb.gameController = GameObject.Find("GameController");
+        fb.slider = GameObject.Find("Slider");
+        fb.overcooked = GameObject.Find("Overcooked").GetComponent<OvercookedDisplayer>();
+    }
+}
